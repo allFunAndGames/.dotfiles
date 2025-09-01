67146b78676ba0264c7fb0c4ab989cd513c558a3
allFun <rob.killilea@gmail.com>
2025-09-01 15:55:48 -0400

    add base dotfiles

---
+++ .oh-my-zsh/custom/shortcut_aliases.zsh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+alias n=nvim
+alias nv=nvim
+alias v=vi
+alias ta=tmux attach
+alias td=tmux detach
+# alias ghidra=/opt/ghidra*/ghidraRun
+function ghidra {
+  /opt/ghidra*/ghidraRun
+}
+alias fd=fdfind
+alias fz=fzf
+alias bat=batcat
+alias hexeditor="hexeditor --nocolor"
